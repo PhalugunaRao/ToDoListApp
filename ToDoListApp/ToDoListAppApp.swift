674651c55cb8cef5d15989c6e276f05cc389c623
PhalgunaRao <phalu900@gmail.com>
2023-06-03 21:46:17 +0530

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by ekincare on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
